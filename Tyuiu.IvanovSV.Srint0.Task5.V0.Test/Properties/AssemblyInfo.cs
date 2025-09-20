e99d95aef1a4f0026ba766748dc20eda3c5d4eb7
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-09-20 10:06:43 +0500

    Task5

---
+++ Tyuiu.IvanovSV.Srint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovSV.Srint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovSV.Srint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5faba8c9-58b4-42eb-a3c6-0e77da729a2d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
